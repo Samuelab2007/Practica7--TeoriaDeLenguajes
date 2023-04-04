df561908696e732dc38182bafc731a544d77c340
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2023-04-04 01:36:00 -0500

    Añade método para calcular el resultado.

--- Parser.py
+++ Parser.py
@@ -1,23 +1,44 @@
+# Idea: TODO: Al realizar el Parseo(construir el árbol), es necesario que al evaluar se pueda extraer cuál es la operación a realizar
+
+
 class Node:
     def evaluate(self):
         pass
 
-class BinOp(Node): # Estudiar como funciona este nodo, si guarda referencias o valores
+
+class BinOp(
+    Node
+):  # Nodo usado para representar expresiones y operaciones en el árbol sintáctico.
     def __init__(self, left, op, right):
         self.left = left
         self.op = op
         self.right = right
-    def evaluate(self):
-        pass # Recorre el arbol, o subarbol y se realiza las operaciones que representa.
 
-class Number(Node):
+    def evaluate(
+        self,
+    ):  # Retorna el valor de la operación de manera recursiva. En caso de que BinOp apunte a otro BinOp.
+        leftterm = self.left.evaluate()
+        rightterm = self.right.evaluate()
+        if self.op == "+":
+            return float(leftterm + rightterm)
+        elif self.op == "-":
+            return float(leftterm - rightterm)
+        elif self.op == "*":
+            return float(leftterm * rightterm)
+        elif self.op == "/":
+            return float(leftterm / rightterm)
+        # Recorre el arbol, o subarbol y se realiza las operaciones que representa.
+
+
+class Number(Node):  # Nodo sencillo con el valor numérico de un token
     def __init__(self, value):
         self.value = value
 
     def evaluate(self):
         return self.value
 
-class Parser:   # Construye un arbol sintáctico, la raiz es una operación
+
+class Parser:  # Construye un arbol sintáctico, la raiz es una operación. Árbol INORDEN
     def __init__(self, tokens):
         self.tokens = tokens
         self.pos = 0
@@ -27,37 +48,45 @@ class Parser:   # Construye un arbol sintáctico, la raiz es una operación
 
     def parse_expression(self):
         left = self.parse_term()
-        while self.pos < len(self.tokens) and self.tokens[self.pos] in ['+', '-']:
-            op = self.tokens[self.pos]
-            self.pos += 1
-            right = self.parse_term()
-            left = BinOp(left, op, right)
+        while self.pos < len(self.tokens) and self.tokens[self.pos] in ["+", "-"]:
+            op = self.tokens[self.pos]  # Guarda el símbolo de la operación
+            self.pos += 1  # Avanza a el otro término de la operación.
+            right = self.parse_term()  # Lado derecho de la operación.
+            left = BinOp(
+                left, op, right
+            )  # Crea un nodo BinOp, con la info del lado izquierdo(ya parseado), la operación y el lado derecho(no parseado)
         return left
 
     def parse_term(self):
         left = self.parse_factor()
-        while self.pos < len(self.tokens) and self.tokens[self.pos] in ['*', '/']:
+        while self.pos < len(self.tokens) and self.tokens[self.pos] in ["*", "/"]:
             op = self.tokens[self.pos]
             self.pos += 1
             right = self.parse_factor()
             left = BinOp(left, op, right)
         return left
 
-    def parse_factor(self):
-        if self.tokens[self.pos] == '(':
+    def parse_factor(self):  # Para los factores que componen un término
+        if (
+            self.tokens[self.pos] == "("
+        ):  # Disminuye la ambiguedad agrupando con paréntesis
             self.pos += 1
-            node = self.parse_expression()
+            node = (
+                self.parse_expression()
+            )  # Parsea la expresión al interior del paréntesis. Y guarda la referencia al subárbol que la representa
             self.pos += 1
             return node
-        else:
-            node = Number(int(self.tokens[self.pos]))
+        else:  # Si no hay paréntesis
+            node = Number(
+                float(self.tokens[self.pos])
+            )  # Crea un nodo Number con la información del número que representa el token
             self.pos += 1
             return node
 
 
-tokens = ['(', '3', '+', '4', ')', '*', '5']
+tokens = ["(", "3", "+", "10", "-", "9", ")", "*", "(", "7", "-", "3", ")"]
 parser = Parser(tokens)
 tree = parser.parse()
-# print(tree.evaluate())
-print(tree.left.right.evaluate())
+print(tree.evaluate())  # Resultado de la operación
+
 # Imprime 35
