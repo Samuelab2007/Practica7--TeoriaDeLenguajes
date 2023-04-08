c2bf2fe6ec7e8177e68c0def433682a2681676e0
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2023-04-07 23:02:29 -0500

    Terminado el Analizador Léxico.

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -6,31 +6,40 @@
 # Salida: Verificación léxica y lista de tokens (token, tipo)
 
 
-def crearToken(
-    value, tipo
-):  # Formato de los tokens, integrarlo en la función de transición
-    return {"valor": value, "tipo": tipo}
-
-
 class AnalizadorLexico:
     def __init__(self, cadena):
         self._cadena = cadena
         self._listaEstados = ["Q0", "Q1"]
         self._estadoActual = "Q0"
         self._terminales = {  # Símbolos terminales, de entrada que acepta el autómata
-            "digitos": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            "digitos": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
             "operadores": ["+", "-", "*", "/"],
         }
         self._pila = []
-        self._valorToken = ""  # Valor construido para el token a generar, cambia con cada operacion en el autómata.
-        self._listaTokens  # Lista de tokens a devolver al final
+        self._valorNumero = ""  # Valor construido para el token a generar, cambia con cada operacion en el autómata.
+        self._recibeOperador = False
+        self._listaTokens = []  # Lista de tokens a devolver al final
+
+    def getListaTokens(self):
+        return self._listaTokens
+
+    def getValorNumero(self):
+        return self._valorNumero
+
+    def resetValorNumero(self):
+        self._valorNumero = ""
+
+    def crearToken(self, value, tipo):
+        tokenCreado = {"tipo": tipo, "valor": value}
+        self._listaTokens.append(tokenCreado)
 
     def transicionDigito(self, simboloEntrada: str):  # Concatena el digito y pasa a Q1
         self._estadoActual = "Q1"
-        self._valorToken += simboloEntrada  # Concatena el digito al numero.
+        self._valorNumero += simboloEntrada  # Concatena el digito al numero.
 
     def transicionDos(self, simboloEntrada: str):
         self._estadoActual = "Q2"
+        self._pila.append(simboloEntrada)
         tipoOperador = ""
         if simboloEntrada == "+":  # Determina el tipo de operador
             tipoOperador = "OPERADOR_SUMA"
@@ -40,7 +49,7 @@ class AnalizadorLexico:
             tipoOperador = "OPERADOR_MULTIPLICACION"
         elif simboloEntrada == "/":
             tipoOperador = "OPERADOR_DIVISION"
-        crearToken(
+        self.crearToken(
             simboloEntrada, tipoOperador
         )  # Crea el token y lo guarda en la lista de tokens
 
@@ -48,16 +57,36 @@ class AnalizadorLexico:
         self._estadoActual = "Q2"
         self._pila.pop()
         self._pila.append(simboloEntrada)
-        crearToken(simboloEntrada, "PARENTESIS_IZQUIERDO")
+        self.crearToken(simboloEntrada, "PARENTESIS_IZQUIERDO")
 
     def transicionCuatro(self, simboloEntrada: str):
         self._estadoActual = "Q2"  # Transición cuatro
+        self._recibeOperador = (
+            True  # Para permitir operadores inmediatamante despues de parentesis.
+        )
         self._pila.pop()
-        crearToken(simboloEntrada, "PARENTESIS_DERECHO")
+        self.crearToken(simboloEntrada, "PARENTESIS_DERECHO")
 
-    def transicionCinco(self):
+    def transicionCinco(self, simboloEntrada):
         self._estadoActual = "Q1"
+        self._valorNumero += simboloEntrada
         self._pila.pop()
+
+    def determinaraceptacion(self):
+        if self._cadena[-1:] in self._terminales["operadores"]:
+            return "Cadena Inválida"
+        for caracter in self._cadena:
+            aceptacioncaracter = self.funcionTransicion(caracter)
+            if aceptacioncaracter == 99:
+                return "Cadena Inválida"
+        if self.getValorNumero() != "":
+            self.crearToken(
+                self.getValorNumero(), "INTEGER"
+            )  # Cuando se agota la cadena y estaba leyendo un numero
+        if len(self._pila) == 0:
+            return "Cadena Válida"
+        else:
+            return "Cadena Inválida"
 
     def funcionTransicion(self, simboloEntrada: str):
         estado = self._estadoActual
@@ -70,15 +99,20 @@ class AnalizadorLexico:
             ):  # La entrada es un numero.
                 self.transicionDigito(simboloEntrada)
             elif simboloEntrada == "(":
+                self.crearToken(simboloEntrada, "PARENTESIS_IZQUIERDO")
                 estado = "Q2"
                 pila.append(simboloEntrada)
             else:
                 return 99
-        if estado == "Q1":  # Función de transición para Q1
+        elif estado == "Q1":  # Función de transición para Q1
             if len(pila) == 0:
                 if simboloEntrada in simbolosTerminales["digitos"]:
                     self.transicionDigito(simboloEntrada)  # Transición Uno
                 elif simboloEntrada in simbolosTerminales["operadores"]:
+                    self.crearToken(
+                        self.getValorNumero(), "INTEGER"
+                    )  # Token con el numero anterior
+                    self.resetValorNumero()  # Borro los digitos ya leídos
                     self.transicionDos(simboloEntrada)  # Transición Dos
                 else:
                     return 99
@@ -86,11 +120,41 @@ class AnalizadorLexico:
                 if simboloEntrada in simbolosTerminales["digitos"]:
                     self.transicionDigito(simboloEntrada)
                 elif simboloEntrada in simbolosTerminales["operadores"]:
+                    self.crearToken(
+                        self.getValorNumero(), "INTEGER"
+                    )  # Crea token con el numero anterior
+                    self.resetValorNumero()  # Resetea el numero almacenado
+                    self.transicionDos(simboloEntrada)
+                elif simboloEntrada == ")":
+                    self.crearToken(self.getValorNumero(), "INTEGER")
+                    self.resetValorNumero()
+                    self.transicionCuatro(simboloEntrada)
+                else:
+                    return 99
+        elif estado == "Q2":  # Función de transición para Q2
+            if len(pila) == 0:
+                if simboloEntrada in simbolosTerminales["operadores"]:
+                    self.transicionDos(simboloEntrada)
+                else:
+                    return 99
+            elif topePila[0] in simbolosTerminales["operadores"]:
+                if simboloEntrada in simbolosTerminales["digitos"]:
+                    self.transicionCinco(simboloEntrada)
+                elif simboloEntrada == "(":
+                    self.transicionTres(simboloEntrada)
+                else:
+                    return 99
+            elif topePila[0] == "(":
+                if simboloEntrada in simbolosTerminales["digitos"]:
+                    self.transicionDigito(simboloEntrada)
+                elif (
+                    simboloEntrada in simbolosTerminales["operadores"]
+                    and self._recibeOperador == True
+                ):
+                    self.transicionDos(simboloEntrada)
+                elif simboloEntrada == "(":
                     self.transicionDos(simboloEntrada)
                 elif simboloEntrada == ")":
                     self.transicionCuatro(simboloEntrada)
                 else:
                     return 99
-
-
-# TODO: Planteamiento formal del autómata
